cadf75baa29fb9d8b7618c993b84d4f0008f8330
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-01-24 17:17:57 -0600

    Adding UI sample app

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
+import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "@auth0/auth0-react";
+import history from "./utils/history";
+import { getConfig } from "./config";
 
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-56pajuydgx8hdw3g.us.auth0.com"
-      clientId="xrdrGrLBmYuGKTqhgIzU98LyO2pApFI0"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
+};
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  onRedirectCallback,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ...(config.audience ? { audience: config.audience } : null),
+  },
+};
+
+ReactDOM.render(
+  <Auth0Provider {...providerConfig}>
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
-reportWebVitals();
+serviceWorker.unregister();
