62b22725e74ae3258e752501e4766b98396223e0
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-01-26 19:58:24 -0600

    Removing unused imports

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { Amplify, API } from "aws-amplify";
 import awsconfig from "../aws-exports";
-import axios from "axios";
 
 Amplify.configure(awsconfig);
 
